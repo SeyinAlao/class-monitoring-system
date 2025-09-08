a3d7735a0fffefb1bdc63795d6d8561bb54416fd
Alao Oluwaseyin <seyinalao@gmail.com>
2025-09-08 15:13:26 +0100

    Added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
-
 import "./globals.css";
-
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = {
   title: "Class Monitoring System",
@@ -15,6 +14,7 @@ export default function RootLayout({ children }) {
        </head>
      <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
